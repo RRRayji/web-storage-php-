ded9c0af302cf6fbe5c5db65b7e3e3827c8842d1
RRRayji <rrrayji@gmail.com>
2024-02-25 20:38:14 +0300

    theme changing

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Hand-made Catalog</title>
+	<title>Storage</title>
 	<!-- <link rel="stylesheet" type="text/css" href="./style/css/style.css"> -->
 	<style>
 		*{
@@ -42,7 +42,7 @@
 		}
 		header{
 			margin: 3svh 2svw 0;
-			background-color: darkcyan;
+			background-color: #4585a9;
 			min-height: 45px;
 			min-width: 570px;
 			height: 5svh;
@@ -53,20 +53,18 @@
 		}
 		.button{
 			width: 15%;
+			min-width: 120px;
 			height: 100%;
-			background-color: #33a4a4;
+			background-color: #92cced;
 			display: flex;
 			justify-content: center;
 			align-items: center;
 			font: 16px Luminari, fantasy;
-			color: rgb(16, 109, 109);/* rgb(82, 136, 138); */
+			color: rgb(41, 78, 97);/* rgb(82, 136, 138); */
 			letter-spacing: 1.3px;
 			text-decoration: none;
 			border: none;
 			cursor: pointer;
-		}
-		.button:nth-of-type(2n){
-			background-color: #2fadad;
 		}
 		.button:first-child{
 			border-radius: 20px 0 0 20px;
@@ -84,7 +82,7 @@
 		main{
 			display: flex;
     		justify-content: center;
-			background-color: rgb(48, 61, 61);
+			background-color: rgb(45, 55, 61);
 			border-radius: 0 0 20px 20px;
 			margin: 0 2svw 3svh;
 			min-height: 400px;
@@ -94,6 +92,7 @@
 		}
 		#scroller{
 			width: 30svw;
+			min-width: 300px;
 			overflow: auto;
 		}
 		.row{
@@ -119,7 +118,7 @@
 			min-height: 100%;
 			min-width: 50px;
 			width: 33.333333%;									/* dynamic */
-			background-color: rgb(8, 124, 124);
+			background-color: #5994c5;
 		}
 		.cell:first-child{
 			border-radius: inherit;
@@ -129,9 +128,8 @@
 			border-radius: inherit;
 			border-bottom-left-radius: 0;
 		}
-		.row:nth-child(odd) .cell:nth-child(even),
-		.row:nth-child(even) .cell:nth-child(odd) {
-			background-color: rgb(8, 100, 100);
+		.row:nth-child(odd) .cell{
+			background-color: #6db5dd;
 		}
 	</style>
 </head>
@@ -141,27 +139,18 @@
 	<header>
 		<button id="add" class="button">ДОБАВИТЬ</button>
 		<form class="button" method="post" action="">
-			<button id="table_name" name="table_name" class="button">НАЗВАНИЕ ТАБЛИЦЫ</button>
+			<select name="table_name" id="table_name" onchange="this.form.submit()">
+				<option value="" name="first_element" id="first_element"></option>
+				<option value="отладочная">отладочная</option>
+				<option value="ед_изм">ед_изм</option>
+				<option value="категория">категория</option>
+			</select>
 		</form>
 		<button id="remove" class="button">УДАЛИТЬ</button>
 	</header>
 	<main>
 		<div id="scroller">
-			<div class="row">
-				<div class="cell">ид</div>
-				<div class="cell">значение</div>
-				<div class="cell">ед_изм</div>
-			</div>
-			<div class="row">
-				<div class="cell">1</div>
-				<div class="cell">10</div>
-				<div class="cell">л</div>
-			</div>
-			<div class="row">
-				<div class="cell">2</div>
-				<div class="cell">15</div>
-				<div class="cell">кг</div>
-			</div>
+			<!-- DATA -->
 		</div>
 	</main>
 	
@@ -172,12 +161,14 @@
 
 <?php
 
-$table_name = $_POST['table_name'];
-#echo "'" . $table_name . "'";
+$table_name = ($_POST["table_name"] == null) ? "отладочная" : $_POST["table_name"];
+echo '<script>
+	document.getElementById(`first_element`).innerHTML = `'. $table_name .'`;
+</script>';
 echo '<script>
 	var scroller = document.getElementById(`scroller`);
-	scroller.innerHTML = `'. Output::get_table_cols("отладочная") .'`;
-	scroller.innerHTML += `'. Output::get_table_data("отладочная") .'`;
+	scroller.innerHTML = `'. Output::get_table_cols($table_name) .'`;
+	scroller.innerHTML += `'. Output::get_table_data($table_name) .'`;
 </script>';
 
 ?>
